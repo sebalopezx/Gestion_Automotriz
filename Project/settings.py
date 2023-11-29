75fc19008b43222a669f21e1076a0411f28ecef5
sebalopezx <se.lopezn@gmail.com>
2023-11-29 16:07:26 -0300

    10. deploy finish test3

--- Project/settings.py
+++ Project/settings.py
@@ -180,12 +180,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # URL base para los archivos multimedia
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 # Ruta al directorio donde se guardarán los archivos multimedia
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
