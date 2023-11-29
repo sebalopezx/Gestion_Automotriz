7e30db5d531b132b1e97c908c15d55ce3cf2f07c
sebalopezx <se.lopezn@gmail.com>
2023-11-29 15:53:08 -0300

    10. deploy finish test

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ set -o errexit
 # poetry install
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Aplicar migraciones
 python manage.py migrate
-
+# Recopilar archivos estáticos
+python manage.py collectstatic --no-input
+# Cargar datos
 python manage.py loaddata data.json
